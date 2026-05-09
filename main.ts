51d79dfa0dd31a3e1f8db036527e49bc399a72ba
WissalJabaoui <wissal.jabaoui@stu.ocsb.ca>
2026-05-09 10:25:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-let random_number = randint(0, 6)
-game.splash("pick a number from 0 to 6!")
-game.splash(game.askForNumber("what is the number?"))
-game.splash(random_number)
-if (true) {
-    game.splash("you got the right number :)")
-} else if (false) {
-    game.splash("you the wrong number :(")
+game.splash("I'm thinking of a number between 1 and 6. Can you guess it?")
+let secret_number = randint(1, 6)
+let user_guess = game.askForNumber("what is your guess?")
+if (secret_number == user_guess) {
+    game.splash("you got it right! :)")
+} else {
+    game.splash("you it wrong. the number was: " + secret_number)
 }
